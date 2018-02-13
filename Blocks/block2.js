95cff1caf0e0c43e49a1072567b56e56c837b94c
Christian Gramling <gramlinc@uci.edu>
2018-02-13 15:55:48 -0800

    Added block scripts and started leveleditor UI

---
+++ Blocks/block2.js
@@ -0,0 +1,32 @@
+addBlock({
+      //Spike
+      id: 2,
+      solid: true,
+      angle: 0,
+      redraws: false,
+      draw: function(canvas, x,y,w,h, world,i,j) {
+        canvas.fillStyle="white";
+        canvas.strokeStyle = "#000";
+        canvas.save();
+        canvas.translate(x+w/2,y+h/2);
+        canvas.rotate(this.angle);
+        // this.angle += Math.PI/20*1.5;
+        w=w*.9;
+        h=h*.9;
+        for(var i=0;i<3;i++){
+          canvas.rotate(Math.PI/8);
+          canvas.fillRect(-w/2,-h/2,w,h);        
+          canvas.strokeRect(-w/2,-h/2,w,h);
+        }
+        w=w*.8;
+        h=h*.8;
+        canvas.rotate(-3*Math.PI/8);
+        canvas.fillStyle="grey";
+        canvas.fillRect(-w/2,-h/2,w,h);      
+        canvas.restore();
+      },
+      entityCollision: function(entity, pos) {
+        if(entity.player) entity.die();
+        return true;
+      }
+});
---
+++ Blocks/index.js
@@ -0,0 +1,7 @@
+loadSequentially('Blocks/',
+[
+  "blockmanager.js",
+  "block1.js",
+  "block2.js",
+  "block3.js",
+]);
