a14c1805557b5b5a6a6fac47abb8d8aad09b12a0
Christian Gramling <gramlinc@uci.edu>
2018-03-17 07:44:00 -0700

    Improved woofer

--- Entities/Woof.js
+++ Entities/Woof.js
@@ -7,30 +7,38 @@ class Woof extends Enemy {
     this.jumpPower = 10;
     this.killPlayer = false;
     this.startY= y;
+    this.push = true;
   }
-  die() {
 
-  }
+  onHitPlayer(player) {
+    
+    player.vy = -5;
+    player.vx = (2*(this.mx >= 0)-1) * 35;
+    super.onHitPlayer(player);
+
+}
+
+
   getHitByEntity(player) {
 		player.BounceOffEntity(this);
 		//this.h=this.h/2;
 		//this.die();
 	}
+
+
   update(dt, frameCount) {
     var dist = this.game.player.x-this.x;
     var close = false;
     var superclose = false;
     if ((dist > -300 && dist < 300)) {
       close = true;
-      if ((dist > -75 && dist < 75)) {
+      if ((dist > -100 && dist < 100)) {
         superclose = true;
       }
     }
     
-    super.update(dt, frameCount);
-
     var ydist = this.game.player.y-this.y;
-    if (close && ydist < 200 && ydist > -100)
+    if (close && ydist < 300 && ydist > -300)
     {
       var left = dist < 0;
       var moveleft = this.mx < 0;
@@ -41,5 +49,7 @@ class Woof extends Enemy {
         this.jump();
       }
     }
+
+    super.update(dt, frameCount);
   }
 }
