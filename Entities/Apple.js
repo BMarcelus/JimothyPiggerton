92e48bc32d349dbf11c3d8179c706407feaea1f9
BMarcelus <bmdizon@uci.edu>
2018-05-15 17:14:36 -0700

    apple sprite

--- Entities/Apple.js
+++ Entities/Apple.js
@@ -1,57 +1,65 @@
 
 class Apple extends Mover {
-    constructor(x,y){
-        super(x,y);
-        this.x=x;this.y=y;
-        this.startY=this.y;
-        this.w= 20;
-        this.h = 20;
-        this.r = 15;
-        this.mx = 0;
-        //this.vy = 0;
-        this.grav = 0;
-        this.color = "red";
-        //this.behind = true;
-        this.hit = false;
-    }
-    update(dt, frameCount) {    
-		  var doinkBox = this.getHitBox();
-      var playerBox = this.game.player.getHitBox();
-      if(!this.hit&&rectangleCollision(doinkBox, playerBox)&&this.game.player.vy>0) {
-        if(this.playerCollision(this.game.player)) {
-          this.getHitByEntity(this.game.player);
-          setTimeout(() => {
-            this.y = this.startY;
-            this.hit = false;
-            this.grav = 0;
-          }, 1000);
-        }
-      }
-      if(this.game.pig&&rectangleCollision(doinkBox, this.game.pig.getHitBox())) {
+  constructor(x,y){
+      super(x,y);
+      this.x=x;this.y=y;
+      this.startY=this.y;
+      this.w= 20;
+      this.h = 20;
+      this.r = 15;
+      this.mx = 0;
+      //this.vy = 0;
+      this.grav = 0;
+      this.color = "red";
+      //this.behind = true;
+      this.hit = false;
+  }
+  update(dt, frameCount) {    
+    var doinkBox = this.getHitBox();
+    var playerBox = this.game.player.getHitBox();
+    if(!this.hit&&rectangleCollision(doinkBox, playerBox)&&this.game.player.vy>0) {
+      if(this.playerCollision(this.game.player)) {
+        this.getHitByEntity(this.game.player);
+        setTimeout(() => {
+          this.y = this.startY;
+          this.hit = false;
           this.grav = 0;
-          this.y -= 5000;
-          this.game.pig.apples++;
+        }, 1000);
       }
-      super.update(dt, frameCount);
-      // if (this.bounceAnimation > 0)this.bounceAnimation-=1;
+    }
+    if(this.game.pig&&rectangleCollision(doinkBox, this.game.pig.getHitBox())) {
+        this.grav = 0;
+        this.y -= 5000;
+        this.game.pig.apples++;
+    }
+    super.update(dt, frameCount);
+    // if (this.bounceAnimation > 0)this.bounceAnimation-=1;
 	}
 
-    getHitByEntity(player) {
-        //this.bounceAnimation = 20;
-        // player.jumpCount--;
-        // player.jump(5);
-        player.bounceOffEntity(this, 15);
-        this.grav = 1;
-        this.hit=true;
-      //player.apples++;
+  getHitByEntity(player) {
+    //this.bounceAnimation = 20;
+    // player.jumpCount--;
+    // player.jump(5);
+    player.bounceOffEntity(this, 15);
+    this.grav = 1;
+    this.hit=true;
+    //player.apples++;
 	}
 
   playerCollision(player) {
 		return true;
 	}
-
-    getHitBox() {
+  getHitBox() {
     return {x:this.x-.5*this.w, y:this.y-this.h, w:this.w, h:this.h};
-    }
-    
+  }
+  drawShape(canvas,w,h) {
+    canvas.fillStyle = '#f00';
+    canvas.fillRect(-w/2,-h,w,h);
+    canvas.fillStyle = "#a00";
+    canvas.fillRect(-w/2,-h,w/2,h);
+    canvas.fillStyle= "#fff";
+    canvas.fillRect(-w*0.1,-h*0.9,w*0.5,h*0.2);
+    canvas.fillStyle = "#640";
+    canvas.fillRect(-w*0.1,-h*1.3,w*0.2,h*0.3);
+  }
 }
